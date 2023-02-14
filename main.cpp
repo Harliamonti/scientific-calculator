e261ec4cbdd17d4777e3e1f605acb335738f8e0a
Harliamonti <123170910+Harliamonti@users.noreply.github.com>
2023-02-14 10:11:35 +0200

    Add templates for correct return types

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 #include <string>
 #include <queue>
+#include <typeinfo>
+#include <cmath>
+
+template <typename T, typename T2>
+auto add(T lhs, T2 rhs)
+{
+    return lhs + rhs;
+}
+
+template <typename T>
+auto foo(T number)
+{
+    if (std::trunc(number) == number)
+        return static_cast<int> (number);
+}
 
 int main()
 {
