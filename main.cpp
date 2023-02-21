966f1ca93760e59d518cf61bab3eab90d8475fac
Harliamonti <123170910+Harliamonti@users.noreply.github.com>
2023-02-21 17:12:29 +0200

    Add basic operation templates

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <queue>
 #include <typeinfo>
 #include <cmath>
+#include <map>
+#include <type_traits>
 
 template <typename T, typename T2>
 auto add(T lhs, T2 rhs)
@@ -19,9 +21,30 @@ auto foo(T number)
 
 namespace calc 
 {
+    template <typename T1, typename T2>
+    auto add(T1 lhs, T2 rhs)
+    {
+        return lhs + rhs;
+    }
+
+    template <typename T1, typename T2>
+    auto multiply(T1 lhs, T2 rhs)
+    {
+        return lhs * rhs;
+    }
+
+    // template <typename T, typename U, typename RT = std::common_type<T, U>>
+    // RT doOperation(T lhs, U rhs, RT (*func)(T arg1, U arg2))
+    // {
+    //     return func(lhs, rhs);
+    // }
     
+    //wont work, rather use an object
+
 }
 int main()
 {
-    
+    int a{ 14 }, b{ 16 };
+
+    std::cout << calc::add(1, 7.5);
 }
