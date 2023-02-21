30de3947537859669f8d972ff3b4466e4cef7f9c
Harliamonti <123170910+Harliamonti@users.noreply.github.com>
2023-02-21 16:17:01 +0200

    Remove examples

--- main.cpp
+++ main.cpp
@@ -17,30 +17,11 @@ auto foo(T number)
         return static_cast<int> (number);
 }
 
+namespace calc 
+{
+    
+}
 int main()
 {
-    std::string input{"1+2*4"};
-    std::queue <int> operands {}; 
-    std::queue <char> operators {};
-
-    int num{};
-
-    for (auto it{input.begin()}; it != input.end(); it++)
-    {
-        std::cout << "Test" << ++num << std::endl;
-    }
-
-    for (std::string ::iterator it{input.begin()}; it != input.end(); it++ )
-    {
-        std::cout << ++num << std::endl;
-    }
-
-    for (auto it : input)
-        std::cout << "test" << num++ << std::endl;
-
-    int a{}, b{10}, x{11};
-
-    b < x 
-        ? std::cout << "True"
-        : std::cout << "False";
+    
 }
